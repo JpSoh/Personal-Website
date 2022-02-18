78b6289da142a67d64fd1d73d3d25f0640cead68
Jp Soh <jpsoh071@gmail.com>
2022-02-18 11:36:50 +0800

    18/2/2022 Ver 4

---
+++ public/login/js/login.js
@@ -0,0 +1,23 @@
+let signInForm = document.querySelector('.sign-in-form');
+
+
+signInForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    let email = document.querySelector('#sign-in-email').value;
+    let password = document.querySelector('#sign-in-password').value;
+    fetch('http://localhost:3000/admin_password/login',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email, password})
+    }).then((resp) => resp.json()).then((data) => {
+        let redirectURL= data.redirectURL;
+        //Redirect to admin page if login is successful
+        if(redirectURL){
+            window.location.href = redirectURL; 
+        } else{
+            alert("Your password and email do not match. Please try again.");
+        }
+    });
+})
